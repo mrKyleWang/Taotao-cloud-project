87c2026333c470d97e25ac5b09da42213341a290
KyleWang <Kyle.Wang.cn@outlook.com>
2018-01-23 20:39:11 +0800

    创建后台管理manager-web, 引入静态页面

---
+++ taotao-manager-web/src/main/java/top/kylewang/taotao/ManagerWebApplication.java
@@ -0,0 +1,14 @@
+package top.kylewang.taotao;
+
+/**
+ * @author Kyle.Wang
+ * 2018/1/23 0023 20:26
+ */
+
+public class ManagerWebApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
