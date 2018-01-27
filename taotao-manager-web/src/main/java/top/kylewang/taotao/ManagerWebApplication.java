9428be34ca1c07d05b85d77c665eb4eb1ff3c365
KyleWang <Kyle.Wang.cn@outlook.com>
2018-01-27 13:01:18 +0800

    修改manager-web静态页面

--- taotao-manager-web/src/main/java/top/kylewang/taotao/ManagerWebApplication.java
+++ taotao-manager-web/src/main/java/top/kylewang/taotao/ManagerWebApplication.java
@@ -1,14 +1,17 @@
 package top.kylewang.taotao;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Kyle.Wang
  * 2018/1/23 0023 20:26
  */
-
+@SpringBootApplication
 public class ManagerWebApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(ManagerWebApplication.class,args);
     }
 }
