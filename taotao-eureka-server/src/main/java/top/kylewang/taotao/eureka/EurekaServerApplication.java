e96db775d793eea8dd8a7a8cdcc54d318c802a65
KyleWang <Kyle.Wang.cn@outlook.com>
2018-01-27 13:00:58 +0800

    新增eureka服务注册中心

---
+++ taotao-eureka-server/src/main/java/top/kylewang/taotao/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package top.kylewang.taotao.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Kyle.Wang
+ * 2018/1/27 0027 11:39
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
