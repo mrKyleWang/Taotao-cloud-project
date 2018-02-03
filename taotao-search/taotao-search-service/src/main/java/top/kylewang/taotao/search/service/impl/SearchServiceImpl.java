41b3763647f58cebc8c6e6be69658a5e0009bef9
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-03 21:09:08 +0800

    修改elasticsearch检索

--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchServiceImpl.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchServiceImpl.java
@@ -1,17 +1,26 @@
 package top.kylewang.taotao.search.service.impl;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.solr.client.solrj.SolrQuery;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
-import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.index.query.WildcardQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.highlight.HighlightBuilder;
+import org.elasticsearch.search.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.stereotype.Service;
+import top.kylewang.taotao.common.pojo.SearchItem;
 import top.kylewang.taotao.common.pojo.SearchResult;
-import top.kylewang.taotao.search.dao.ItemSearchDao;
 import top.kylewang.taotao.search.index.SearchRepository;
 import top.kylewang.taotao.search.service.SearchService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 商品搜索服务实现类
@@ -25,80 +34,109 @@ import top.kylewang.taotao.search.service.SearchService;
 public class SearchServiceImpl implements SearchService {
 
     @Autowired
-    private ItemSearchDao itemSearchDao;
+    private SearchRepository searchRepository;
 
     @Autowired
-    private SearchRepository searchRepository;
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @Override
     public SearchResult search(String queryString, int page, int rows) throws Exception {
+
+        if (page < 1) { page = 1; }
+
         BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
-        // 运单号等值查询
-        if (StringUtils.isNotBlank(wayBill.getWayBillNum())) {
-            TermQueryBuilder wayBillNumQuery = new TermQueryBuilder("wayBillNum", wayBill.getWayBillNum());
-            queryBuilder.must(wayBillNumQuery);
-        }
-        // 发货地模糊查询
-        if (StringUtils.isNotBlank(wayBill.getSendAddress())) {
-            // 情况1 : 条件本身是词条一部分, 直接进行模糊查询
-            WildcardQueryBuilder sendAddressWildcardQuery = new WildcardQueryBuilder("sendAddress", "*" + wayBill.getSendAddress() + "*");
-            // 情况2 : 条件需要分词后再进行词条匹配, 取交集(AND)
-            QueryStringQueryBuilder sendAddressQueryStringQuery = new QueryStringQueryBuilder(wayBill.getSendAddress()).field("sendAddress").defaultOperator(QueryStringQueryBuilder.Operator.AND);
-            // 对两种情况下的查询取or 关系(should)
-            BoolQueryBuilder sendAddressQuery = new BoolQueryBuilder();
-            sendAddressQuery.should(sendAddressWildcardQuery);
-            sendAddressQuery.should(sendAddressQueryStringQuery);
-            queryBuilder.must(sendAddressQuery);
-        }
-        // 收货地模糊查询
-        if (StringUtils.isNotBlank(wayBill.getRecAddress())) {
-            // 情况1 : 条件本身是词条一部分, 直接进行模糊查询
-            WildcardQueryBuilder recAddressWildcardQuery = new WildcardQueryBuilder("recAddress", "*" + wayBill.getRecAddress() + "*");
-            // 情况2 : 条件需要分词后再进行词条匹配, 取交集(AND)
-            QueryStringQueryBuilder recAddressQueryStringQuery = new QueryStringQueryBuilder(wayBill.getRecAddress()).field("recAddress").defaultOperator(QueryStringQueryBuilder.Operator.AND);
-            // 对两种情况下的查询取or 关系(should)
-            BoolQueryBuilder recAddressQuery = new BoolQueryBuilder();
-            recAddressQuery.should(recAddressWildcardQuery);
-            recAddressQuery.should(recAddressQueryStringQuery);
-            queryBuilder.must(recAddressQuery);
-        }
-        // 快递产品类型等值查询
-        if (StringUtils.isNotBlank(wayBill.getSendProNum())) {
-            TermQueryBuilder sendProNumQuery = new TermQueryBuilder("sendProNum", wayBill.getSendProNum());
-            queryBuilder.must(sendProNumQuery);
-        }
-        // 运单状态等值查询
-        if (wayBill.getSignStatus() != null && wayBill.getSignStatus() != 0) {
-            TermQueryBuilder signStatusQuery = new TermQueryBuilder("signStatus", wayBill.getSignStatus());
-            queryBuilder.must(signStatusQuery);
+
+        // title
+        // 情况1 : 条件本身是词条一部分, 直接进行模糊查询
+        WildcardQueryBuilder titleWildcardQuery = new WildcardQueryBuilder("title", "*" + queryString + "*");
+        // 情况2 : 条件需要分词后再进行词条匹配, 取交集(AND)
+        QueryStringQueryBuilder titleQueryStringQuery = new QueryStringQueryBuilder(queryString).field("title").defaultOperator(QueryStringQueryBuilder.Operator.AND);
+        // sell_point
+        // 情况1 : 条件本身是词条一部分, 直接进行模糊查询
+        WildcardQueryBuilder sellPointWildcardQuery = new WildcardQueryBuilder("sell_point", "*" + queryString + "*");
+        // 情况2 : 条件需要分词后再进行词条匹配, 取交集(AND)
+        QueryStringQueryBuilder sellPointQueryStringQuery = new QueryStringQueryBuilder(queryString).field("sell_point").defaultOperator(QueryStringQueryBuilder.Operator.AND);
+        // title
+        // 情况1 : 条件本身是词条一部分, 直接进行模糊查询
+        WildcardQueryBuilder descWildcardQuery = new WildcardQueryBuilder("item_des", "*" + queryString + "*");
+        // 情况2 : 条件需要分词后再进行词条匹配, 取交集(AND)
+        QueryStringQueryBuilder descQueryStringQuery = new QueryStringQueryBuilder(queryString).field("item_des").defaultOperator(QueryStringQueryBuilder.Operator.AND);
+
+        // 对查询取or 关系(should)
+        BoolQueryBuilder itemQuery = new BoolQueryBuilder();
+        itemQuery.should(titleWildcardQuery);
+        itemQuery.should(titleQueryStringQuery);
+        itemQuery.should(sellPointWildcardQuery);
+        itemQuery.should(sellPointQueryStringQuery);
+        itemQuery.should(descWildcardQuery);
+        itemQuery.should(descQueryStringQuery);
+
+        queryBuilder.must(itemQuery);
+
+        //设置高亮显示
+        HighlightBuilder highlightBuilder = new HighlightBuilder().field("*").requireFieldMatch(false);
+        highlightBuilder.preTags("<span style=\"color:red\">");
+        highlightBuilder.postTags("</span>");
+
+        SearchRequestBuilder searchRequestBuilder = elasticsearchTemplate.getClient()
+                .prepareSearch("taotao").setTypes("item").setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setHighlighterPreTags("<span style=\"color:red\">").setHighlighterPostTags("</span>").setQuery(queryBuilder)
+                .setFrom((page-1)*rows).setSize(rows);
+
+        SearchResponse searchResponse = searchRequestBuilder.get();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        //接受结果
+        List<SearchItem> itemList = new ArrayList<>();
+        //遍历结果
+        for(SearchHit hit:searchResponse.getHits()){
+            SearchItem searchItem = new SearchItem();
+            Map<String, Object> source = hit.getSource();
+            searchItem.setId(Long.getLong(source.get("id").toString()));
+            searchItem.setTitle(source.get("title").toString());
+            searchItem.setSell_point(source.get("sell_point").toString());
+            searchItem.setPrice(Long.getLong(source.get("price").toString()));
+            searchItem.setImage(source.get("image").toString());
+            searchItem.setCategory_name(source.get("category_name").toString());
+            searchItem.setItem_des(source.get("item_des").toString());
+
+            //处理高亮片段
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            HighlightField titleField = highlightFields.get("title");
+            HighlightField sellPointField = highlightFields.get("sell_point");
+            HighlightField itemDesField = highlightFields.get("item_des");
+            if(titleField!=null){
+                Text[] fragments = titleField.fragments();
+                String tmp ="";
+                for(Text text:fragments){
+                    tmp+=text;
+                }
+                searchItem.setTitle(tmp);
+            }
+            if(sellPointField!=null){
+                Text[] fragments = sellPointField.fragments();
+                String tmp ="";
+                for(Text text:fragments){
+                    tmp+=text;
+                }
+                //将高亮片段组装到结果中去
+                searchItem.setSell_point(tmp);
+            }
+            if(itemDesField!=null){
+                Text[] fragments = itemDesField.fragments();
+                String tmp ="";
+                for(Text text:fragments){
+                    tmp+=text;
+                }
+                //将高亮片段组装到结果中去
+                searchItem.setItem_des(tmp);
+            }
+            itemList.add(searchItem);
         }
 
-
-        // 1、创建一个SolrQuery对象。
-        SolrQuery query = new SolrQuery();
-        // 2、设置查询条件
-        query.setQuery(queryString);
-        // 3、设置分页条件
-        if (page < 1) page = 1;
-        query.setStart((page - 1) * rows);
-        query.setRows(rows);
-        // 4、需要指定默认搜索域。
-        query.set("df", "item_title");
-        // 5、设置高亮
-        query.setHighlight(true);
-        query.addHighlightField("item_title");
-        query.setHighlightSimplePre("<em style=\"color:red\">");
-        query.setHighlightSimplePost("</em>");
-        // 6、执行查询，调用SearchDao。得到SearchResult
-        SearchResult searchResult = itemSearchDao.search(query);
-        // 7、需要计算总页数。
-        long recordCount = searchResult.getRecordCount();
-        long pageCount = recordCount / rows;
-        if (recordCount % rows > 0) {
-            pageCount++;
-        }
-        searchResult.setPageCount(pageCount);
-        // 返回SearchResult
+        SearchResult searchResult = new SearchResult();
+        searchResult.setItemList(itemList);
+        searchResult.setPageCount((searchResponse.getSuccessfulShards()/rows)+1);
+        searchResult.setRecordCount(searchResponse.getSuccessfulShards());
         return searchResult;
     }
 
