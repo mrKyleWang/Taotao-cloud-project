37a552e8d1a880c6826588fea36ac2039d6faa90
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-05 22:17:04 +0800

    引入boot-starter-solr

--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/listener/ItemAddListener.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/listener/ItemAddListener.java
@@ -3,15 +3,12 @@ package top.kylewang.taotao.search.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import top.kylewang.taotao.common.pojo.SearchItem;
-import top.kylewang.taotao.search.index.SearchRepository;
 import top.kylewang.taotao.search.mapper.ItemMapper;
 
 public class ItemAddListener{
 
 	@Autowired
 	private ItemMapper itemMapper;
-	@Autowired
-	private SearchRepository searchRepository;
 
 	@RabbitListener(queues="item-add-topic")
 	public void onMessage(String message) {
@@ -21,8 +18,7 @@ public class ItemAddListener{
 			Long itemId = new Long(message);
 			//根据商品Id查询商品消息
 			SearchItem searchItem = itemMapper.getItemById(itemId);
-			//把商品消息添加到索引库
-			searchRepository.save(searchItem);
+			//把商品消息添加到索引库  TODO 索引操作
 
 		} catch (Exception e) {
 			e.printStackTrace();
--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchItemServiceImpl.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchItemServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import top.kylewang.taotao.common.pojo.SearchItem;
 import top.kylewang.taotao.common.pojo.TaotaoResult;
-import top.kylewang.taotao.search.index.SearchRepository;
 import top.kylewang.taotao.search.mapper.ItemMapper;
 import top.kylewang.taotao.search.service.SearchItemService;
 
@@ -21,15 +20,12 @@ import top.kylewang.taotao.search.service.SearchItemService;
 public class SearchItemServiceImpl implements SearchItemService {
 
 	@Autowired
-	private SearchRepository searchRepository;
-	@Autowired
 	private ItemMapper itemMapper;
 	@Override
 	public TaotaoResult importAllItemToIndex() throws Exception{
 		// 1、查询所有商品数据。
 		List<SearchItem> itemList = itemMapper.getItemList();
-		// 2、添加文档
-		searchRepository.save(itemList);
+		// 2、添加文档 TODO 索引操作
 		// 3、返回TaotaoResult。
 		return TaotaoResult.ok();
 	}
