20f1bca4268c8d054138741a8f15efb16e5c04f6
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-06 16:54:27 +0800

    修改商品添加消息监听

--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/listener/ItemAddListener.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/listener/ItemAddListener.java
@@ -1,24 +1,47 @@
 package top.kylewang.taotao.search.listener;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.common.SolrInputDocument;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import top.kylewang.taotao.common.pojo.SearchItem;
 import top.kylewang.taotao.search.mapper.ItemMapper;
+
+/**
+ * 商品添加监听器
+ * @author Kyle.Wang
+ * 2018/2/6 0006 16:39
+ */
 
 public class ItemAddListener{
 
 	@Autowired
 	private ItemMapper itemMapper;
 
+	@Autowired
+	private SolrClient solrClient;
+
 	@RabbitListener(queues="item-add-topic")
-	public void onMessage(String message) {
-		// 从消息中取商品id
+	public void onMessage(String strItemId) {
 		try {
-			//转换成Long
-			Long itemId = new Long(message);
+			//从消息中取商品id转换成Long
+			Long itemId = new Long(strItemId);
 			//根据商品Id查询商品消息
 			SearchItem searchItem = itemMapper.getItemById(itemId);
-			//把商品消息添加到索引库  TODO 索引操作
+			//把商品消息添加到索引库
+			SolrInputDocument document = new SolrInputDocument();
+			// 4、为文档添加域
+			document.addField("id", searchItem.getId());
+			document.addField("item_title", searchItem.getTitle());
+			document.addField("item_sell_point", searchItem.getSell_point());
+			document.addField("item_price", searchItem.getPrice());
+			document.addField("item_image", searchItem.getImage());
+			document.addField("item_category_name", searchItem.getCategory_name());
+			document.addField("item_desc", searchItem.getItem_des());
+			// 5、向索引库中添加文档。
+			solrClient.add(document);
+			//提交
+			solrClient.commit();
 
 		} catch (Exception e) {
 			e.printStackTrace();
