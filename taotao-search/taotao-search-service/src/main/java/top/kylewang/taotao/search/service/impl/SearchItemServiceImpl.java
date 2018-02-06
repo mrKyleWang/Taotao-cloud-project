b632a1f4f5cad9d267eda2ddf99b18d0ded12ecc
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-06 16:54:12 +0800

    springboot整合solr

--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchItemServiceImpl.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchItemServiceImpl.java
@@ -1,14 +1,16 @@
 package top.kylewang.taotao.search.service.impl;
 
-import java.util.List;
-
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import top.kylewang.taotao.common.pojo.SearchItem;
 import top.kylewang.taotao.common.pojo.TaotaoResult;
 import top.kylewang.taotao.search.mapper.ItemMapper;
 import top.kylewang.taotao.search.service.SearchItemService;
+
+import java.util.List;
 
 /**
  * 导入商品数据到索引库
@@ -20,13 +22,32 @@ import top.kylewang.taotao.search.service.SearchItemService;
 public class SearchItemServiceImpl implements SearchItemService {
 
 	@Autowired
+	private SolrClient solrClient;
+	@Autowired
 	private ItemMapper itemMapper;
+
 	@Override
 	public TaotaoResult importAllItemToIndex() throws Exception{
 		// 1、查询所有商品数据。
 		List<SearchItem> itemList = itemMapper.getItemList();
-		// 2、添加文档 TODO 索引操作
-		// 3、返回TaotaoResult。
+		// 2、创建一个SolrServer对象。
+		for (SearchItem searchItem : itemList) {
+			// 3、为每个商品创建一个SolrInputDocument对象。
+			SolrInputDocument document = new SolrInputDocument();
+			// 4、为文档添加域
+			document.addField("id", searchItem.getId());
+			document.addField("item_title", searchItem.getTitle());
+			document.addField("item_sell_point", searchItem.getSell_point());
+			document.addField("item_price", searchItem.getPrice());
+			document.addField("item_image", searchItem.getImage());
+			document.addField("item_category_name", searchItem.getCategory_name());
+			document.addField("item_desc", searchItem.getItem_des());
+			// 5、向索引库中添加文档。
+			solrClient.add(document);
+		}
+		//提交
+		solrClient.commit();
+		// 6、返回TaotaoResult。
 		return TaotaoResult.ok();
 	}
 
--- taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchServiceImpl.java
+++ taotao-search/taotao-search-service/src/main/java/top/kylewang/taotao/search/service/impl/SearchServiceImpl.java
@@ -1,7 +1,11 @@
 package top.kylewang.taotao.search.service.impl;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.kylewang.taotao.common.pojo.SearchResult;
+import top.kylewang.taotao.search.dao.ItemSearchDao;
 import top.kylewang.taotao.search.service.SearchService;
 
 /**
@@ -15,12 +19,40 @@ import top.kylewang.taotao.search.service.SearchService;
 @Service
 public class SearchServiceImpl implements SearchService {
 
+    @Autowired
+    private ItemSearchDao itemSearchDao;
 
+    @Autowired
+    SolrClient solrClient;
 
     @Override
     public SearchResult search(String queryString, int page, int rows) throws Exception {
-
-        return null;
+        // 1、创建一个SolrQuery对象。
+        SolrQuery query  = new SolrQuery();
+        // 2、设置查询条件
+        query.setQuery(queryString);
+        // 3、设置分页条件
+        if (page < 1) page = 1;
+        query.setStart((page -1) * rows);
+        query.setRows(rows);
+        // 4、需要指定默认搜索域。
+        query.set("df", "item_title");
+        // 5、设置高亮
+        query.setHighlight(true);
+        query.addHighlightField("item_title");
+        query.setHighlightSimplePre("<em style=\"color:red\">");
+        query.setHighlightSimplePost("</em>");
+        // 6、执行查询，调用SearchDao。得到SearchResult
+        SearchResult searchResult = itemSearchDao.search(query);
+        // 7、需要计算总页数。
+        long recordCount = searchResult.getRecordCount();
+        long pageCount = recordCount / rows;
+        if (recordCount % rows > 0) {
+            pageCount++;
+        }
+        searchResult.setPageCount(pageCount);
+        // 返回SearchResult
+        return searchResult;
     }
 
 }
