91f649c7e1186264a5f30e84570d5ebf40ff3b0d
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-12 17:57:09 +0800

    暴露sso-service服务

---
+++ taotao-sso/taotao-sso-service/src/main/java/top/kylewang/taotao/sso/controller/UserLoginController.java
@@ -0,0 +1,35 @@
+package top.kylewang.taotao.sso.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.kylewang.taotao.common.pojo.TaotaoResult;
+import top.kylewang.taotao.sso.service.UserLoginService;
+
+/**
+ * @author Kyle.Wang
+ * 2018/2/12 0012 17:51
+ */
+public class UserLoginController {
+
+    @Autowired
+    private UserLoginService userLoginService;
+
+
+    @RequestMapping(value="/user/login", method= RequestMethod.POST)
+    @ResponseBody
+    public TaotaoResult userLogin(String username, String password) {
+        return userLoginService.login(username,password);
+    }
+
+    @RequestMapping(value="/user/token/{token}")
+    @ResponseBody
+    public Object getUserByToken(@PathVariable String token) {
+       return userLoginService.getUserByToken(token);
+    }
+
+
+
+}
---
+++ taotao-sso/taotao-sso-service/src/main/java/top/kylewang/taotao/sso/controller/UserRegisterCotroller.java
@@ -0,0 +1,32 @@
+package top.kylewang.taotao.sso.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.kylewang.taotao.common.pojo.TaotaoResult;
+import top.kylewang.taotao.pojo.TbUser;
+import top.kylewang.taotao.sso.service.UserRegisterService;
+
+/**
+ * @author Kyle.Wang
+ * 2018/2/12 0012 17:51
+ */
+public class UserRegisterCotroller {
+
+    @Autowired
+    private UserRegisterService userRegisterService;
+
+    @RequestMapping(value="/user/check/{param}/{type}", method= RequestMethod.GET)
+    @ResponseBody
+    public TaotaoResult checkUserInfo(@PathVariable String param, @PathVariable Integer type) {
+        return userRegisterService.checkUserInfo(param,type);
+    }
+
+    @RequestMapping(value="/user/register", method=RequestMethod.POST)
+    @ResponseBody
+    public TaotaoResult regitsterUser(TbUser user) {
+        return userRegisterService.createUser(user);
+    }
+}
