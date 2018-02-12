a8bba0204d167656a2bfad610551ccb9eb69adb1
KyleWang <Kyle.Wang.cn@outlook.com>
2018-02-12 17:51:12 +0800

    整合yml配置

--- taotao-sso/taotao-sso-service/src/main/java/top/kylewang/taotao/SSOServiceApplication.java
+++ taotao-sso/taotao-sso-service/src/main/java/top/kylewang/taotao/SSOServiceApplication.java
@@ -1,9 +1,13 @@
 package top.kylewang.taotao;
 
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 /**
  * @author Kyle.Wang
@@ -18,4 +22,20 @@ public class SSOServiceApplication {
         SpringApplication.run(SSOServiceApplication.class, args);
     }
 
+    /**
+     * 配置mybatis的分页插件pageHelper
+     * @return
+     */
+    @Bean
+    public PageHelper pageHelper() {
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum", "true");
+        properties.setProperty("rowBoundsWithCount", "true");
+        properties.setProperty("reasonable", "true");
+        //配置mysql数据库的方言
+        properties.setProperty("dialect", "mysql");
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
 }
